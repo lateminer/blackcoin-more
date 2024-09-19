7ac04cc21b8a8bdc6e1d10c7f546329fa7bf4acb
lateminer <9951982+lateminer@users.noreply.github.com>
2024-09-19 23:31:31 +0200

    testnet: Activate SegWit on Sep 20, 2024

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -229,8 +229,8 @@ public:
 
         // Deployment of SegWit (BIP141, BIP143, and BIP147)
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].bit = 1;
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1726272000; // Wed Sep 14 2024 00:00:00 GMT+0000
-        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = 0;
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nStartTime = 1726790400; // Wed Sep 20 2024 00:00:00 GMT+0000
+        consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout = Consensus::BIP9Deployment::NO_TIMEOUT;
         consensus.vDeployments[Consensus::DEPLOYMENT_SEGWIT].min_activation_height = 0; // No activation delay
 
         // Deployment of Taproot (BIPs 340-342)
